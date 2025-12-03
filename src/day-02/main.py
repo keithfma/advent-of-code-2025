8030e6a1e9f07154c7e0a493748e3dec23e36b2a
Personal <personal@Mac.hsd1.nh.comcast.net>
2025-12-03 14:08:07 -0500

    Fixed alternate day 2 part 1

--- src/day-02/main.py
+++ src/day-02/main.py
@@ -1,10 +1,10 @@
 from math import log10, ceil
-from itertools import cycle
+from itertools import cycle, chain
 from functools import lru_cache
 
 
 EXAMPLE = 'example.txt'
-REAL = 'input.txt'
+REAL = 'input.txt.bak'
 
 
 def parse_ranges(path: str) -> tuple[tuple[int, int]]:
@@ -18,35 +18,37 @@ def parse_ranges(path: str) -> tuple[tuple[int, int]]:
     return tuple(ranges)
 
 
-def invalid_ids_in_range(first: int, last: int) -> int:
-    """Return sum of invalid IDs in the range first->last inclusive"""
+def invalid_ids_in_range(first: int, last: int) -> list[int]:
     assert first < last and first > 0
 
     invalid = 0
+    all = []
     current = first
     while current <= last:
         
         num_digits = int(log10(current)) + 1 
-        print(f'{current=}, {num_digits=}', end='')
+        # print(f'{current=}, {num_digits=}', end='')
         
         if num_digits % 2:
             # no invalid ID has an odd number of digits, advance to the next value with 
             #   an even number of digits
             current = 10**(num_digits)
-            print(f' -> {current=}')
+            # print(f' -> {current=}')
             continue
 
         factor = 10**(num_digits // 2)  # already know it's even 
         left = current // factor
         right = current - left*factor
-        print(f', {left=}, {right=}', end='')
+        # print(f', {left=}, {right=}', end='')
 
         if left == right:
             # found one! increment the counter
+            all.append(current)
             invalid += current
-            print(f' -> INVALID -> ', end='')
+            # print(f' -> INVALID -> ', end='')
         else:
-            print(f' -> VALID -> ', end='')
+            # print(f' -> VALID -> ', end='')
+            pass
 
 
         if left <= right:
@@ -61,9 +63,9 @@ def invalid_ids_in_range(first: int, last: int) -> int:
 
         # recombine (modified) left and right sides
         current = left*factor + right
-        print(f'{left=}, {right=}, {current=}')
+        # print(f'{left=}, {right=}, {current=}')
     
-    return invalid
+    return all
 
 
 
@@ -102,7 +104,13 @@ def invalid_ids_in_range(first: int, last: int) -> int:
 
 @lru_cache
 def num_digits(x: int) -> int:
-    return int(log10(x)) + 1 
+    try:
+        return int(log10(x)) + 1 
+    except ValueError:
+        if x == 0:
+            return 1
+        raise
+    
 
 
 def repeat(template: int, num_times: int) -> int:
@@ -127,17 +135,41 @@ def repeat(template: int, num_times: int) -> int:
 #     return ceil(n / nx)
 
 
-def next_larger_invalid_id(x: int, n: int) -> int:
+def _next_larger_invalid_id(x: int, n: int) -> int:
     """Return the next next invalid ID larger than x with a repeating pattern of n digits"""
     nx = num_digits(x)
-    num_parts = ceil(nx / n)
-    head = x // 10 ** (nx - n)
+    num_parts = max(2, ceil(nx / n))
+    # TOOD: this ain't quite right. we are grabbing too many digits when the length of the number is odd.
+    head = min(x, x // 10 ** (nx - n))
+
     y = repeat(head, num_parts)
     if y <= x: 
         y = repeat(head + 1, num_parts)
+
+    print(f'{x=}, {n=}, {nx=}, {num_parts=}, {head=}, {y=}')
+
     return y
 
 
+def next_larger_invalid_id(x: int, n: int) -> int:
+    """Return the next integer larger than x that is composed of a pattern which repeats n times"""
+    nx = num_digits(x)
+    np, rem = divmod(nx, n)
+    if rem:
+        # the input number does not divide into n parts evenly, the next pattern that can create an invalid ID with
+        #   n parts must have an extra digit
+        pattern = 10**np
+    else:
+        # the input number divides into n parts evenly, take the first digits as the patter
+        pattern = x // 10 ** (nx - np)
+
+    y = repeat(pattern, n)
+    if y <= x: 
+        y = repeat(pattern + 1, n)
+
+    print(f'{x=}, {n=}, {nx=}, {np=}, {pattern=}, {y=}')
+
+    return y
 
 # def next_larger_invalid_id(x: int) -> int:
 #     """Return the next next invalid ID larger than x with a repeating pattern of n digits"""
@@ -158,28 +190,40 @@ def next_larger_invalid_id(x: int, n: int) -> int:
 #     return result
 
 
+def part_1a(ranges: tuple[tuple[int, int]]):
+    """OG version, which works"""
+    return list(chain(*[invalid_ids_in_range(*r) for r in ranges]))
 
-def part_1(ranges: tuple[tuple[int, int]]):
-    """TODO"""
+
+def part_1b(ranges: tuple[tuple[int, int]]):
+    """Return sum of invalid IDs in all input ranges
+    IDs are invalid if they consist of the same integer repeated 2x
+    """
     total = 0
+    all = []
     for first, last in ranges:
 
         current = first - 1
         
         while True:
-            current = next_larger_invalid_id(current, ceil(num_digits(current) / 2))
+            current = next_larger_invalid_id(current, 2)
             if current > last:
                 break
             total += current
+            all.append(current)
     
-    return total
-
+    return all
 
 
 if __name__ == '__main__':
 
     ranges = parse_ranges(REAL)
-    print(f'Part 1: {sum(invalid_ids_in_range(*r) for r in ranges)}')
+    # ranges = parse_ranges(EXAMPLE)
+    a = part_1a(ranges)
+    b = part_1b(ranges)
+    for aa, bb in zip(a, b):
+        print(f'{aa=}, {bb=}')
+    # print(f'Part 1: {part_1(ranges)}')
 
     # x = 1000
     # for _ in range(1000):
