829d758aa64a59274ad9c0ada893ea09a980260c
Personal <personal@Mac.hsd1.nh.comcast.net>
2025-12-07 11:56:49 -0500

    Day 6 Part 2

--- src/day-06/main.py
+++ src/day-06/main.py
@@ -8,7 +8,7 @@ REAL = 'input.txt'
 OPERATION_MAP: dict[str, np.ufunc] = {'+': np.add, '*': np.multiply}
 
 
-def parse(path: str) -> tuple[np.ndarray, list[np.ufunc]]:
+def do_some_kids_homework(path: str) -> tuple[np.ndarray, list[np.ufunc]]:
     with open(path, 'r') as f:
         lines = f.readlines()
     
@@ -19,19 +19,40 @@ def parse(path: str) -> tuple[np.ndarray, list[np.ufunc]]:
 
     ops = [OPERATION_MAP[x] for x in lines[-1].strip().split()]
 
-    return nums, ops
-
-
-def do_some_kids_homework(nums: np.ndarray, ops: np.ufunc):
     total = 0
     for idx in range(len(ops)):
         total += ops[idx].reduce(nums[:, idx])
+
     return total
 
 
+def do_some_kids_homework_correctly(path: str) -> tuple[np.ndarray, list[np.ufunc]]:
+    with open(path, 'r') as f:
+        lines = [x.rstrip('\n') for x in f.readlines()]
+
+    # transpose the lines with numbers in them to get the "problems", which are sequences of ints
+    problems = []
+    problem = []
+    for digits in zip(*lines[:-1]):
+        try:
+            problem.append( int(''.join(digits)) )
+        except ValueError:
+            # next problem!
+            problems.append(problem)
+            problem = []
+    # don't forget the last one
+    problems.append(problem)
+    
+    ops = [OPERATION_MAP[x] for x in lines[-1].strip().split()]
+
+    total = 0
+    for problem, op in zip(problems, ops):
+        total += op.reduce(problem)
+    
+    return int(total)
+    
 
 if __name__ == '__main__':
 
-    # numbers, operators = parse(EXAMPLE)
-    numbers, operators = parse(REAL)
-    print(f'Part 1: {do_some_kids_homework(numbers, operators)}')
+    print(f'Part 1: {do_some_kids_homework(REAL)}')
+    print(f'Part 2: {do_some_kids_homework_correctly(REAL)}')
